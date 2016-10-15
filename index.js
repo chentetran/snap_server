c1817900d9a2a7930000115bc4c4ae65aad33928
chentetran <vincenttran23@gmail.com>
2016-10-14 21:44:50 -0400

    asdf

---
+++ index.js
@@ -0,0 +1,17 @@
+// Initialization
+var express = require('express');
+var bodyParser = require('body-parser'); // Required if we need to use HTTP query or post parameters
+var firebase = require("firebase");
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP query or post parameters
+
+app.get('/', function() {
+	console.log("hello")
+})
+
+app.listen(app.get('port'), function() {
+
+});
