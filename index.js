e84b40e439c460b4279b02de79285eb6320e8c09
chentetran <vincenttran23@gmail.com>
2016-10-15 01:34:37 -0400

    Asfd

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res) {
 
 // This route is pinged when someone votes to start
 // Takes a userID, gameID
-app.post('/vote', (req, res) => {
+app.post('/vote', function(req, res) {
 	var userID = req.body.userID;
 	var gameID = req.body.gameID;
 
@@ -33,15 +33,50 @@ app.post('/vote', (req, res) => {
 	// Change status to 1 (aka ready)
 	gameRef.child('players').child(userID).child('status').set("1");
 
-	// Get values numPlayers and numReady
-	gameRef.child('numPlayers').once('value', numPlayersSnapshot => {
-		var numPlayers = numPlayersSnapshot.val();
+	gameRef.once('value').then(function(snapshot) {
+		var numPlayers    = snapshot.val().numPlayers;
+		var numReady      = snapshot.val().numReady;
+		var playersInGame = snapshot.val().players;
+		numReady++;
 
-		console.log(numPlayers);
+		gameRef.child("numReady").set(numReady);
+
+		if (numReady / numPlayers > .5) {
+			console.log("[+] Game " + gameID + " is ready to start");
+
+			// Get all players as an array
+			var players = [];
+			// console.log(snapshot.val());
+			playersInGame.forEach(function(snapshot) {
+				console.log(snapshot.val())
+				players.push(snapshot.child('name').val());
+			});
+
+			// console.log(players);
+
+			sattoloCycle(players);
+
+			// Iterate thru players child and assign a target
+			var i = 0;
+			playersInGame.forEach(function(snapshot) {
+				snapshot.ref.child('target').set(players[i]);
+				i++;
+			});
+
+		}
 	});
 
-	res.send("cool")
+	res.send('cool');
 });
+
+function sattoloCycle(items) {
+  for(var i = items.length; i-- > 1; ) {
+    var j = Math.floor(Math.random() * i);
+    var tmp = items[i];
+    items[i] = items[j];
+    items[j] = tmp;
+  }
+}
 
 app.listen(app.get('port'), function() {
 
